f7c1082f5b72af11a4f82d369bb30cfdceef45be
saarang <agarwalsaarang@yahoo.com>
2022-09-17 16:33:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,25 +13,25 @@ def imageRead():
         content = imageFile.read()
 
     image = types.Image(content= content)
-
     response = client.label_detection(image=image)
-
     labels = response.label_annotations
-
     limit = 0
-
     predictions = []
     
     for label in labels:
         if label.score > limit:
             predictions.append(label.description.upper())
+            
     foodInfo = pickle.load(open("inputFiles/foodData.pickle", "rb"))
     actualPred = set()
+    
     for i in predictions:
         if i in foodInfo:
             actualPred.add(i)
+            
     actualPred = list(actualPred)
     print("In your Fridge:")
     print(actualPred)
     print("Recipes:")
     print(getDish(actualPred))
+    
