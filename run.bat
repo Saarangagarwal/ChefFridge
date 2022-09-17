b1b135c675807eb5757e81629165f7e8d629afd6
saarang <agarwalsaarang@yahoo.com>
2022-09-17 16:33:47 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -3,4 +3,4 @@ ECHO running image detection
 python createGroups.py
 set GOOGLE_APPLICATION_CREDENTIALS = C:\Users\16478\Desktop\starterhacks\My First Project-b330c7d7e977.json
 python getter.py
-PAUSE
+PAUSE 
